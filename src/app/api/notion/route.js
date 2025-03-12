a3eda3759d0777a08e341a2fb77bc169691364dd
DESKTOP-D7I9CBB\Sulman <msulman147@gmail.com>
2025-03-12 17:06:09 +0500

    add env

--- src/app/api/notion/route.js
+++ src/app/api/notion/route.js
@@ -4,7 +4,7 @@ export async function GET() {
     {
       method: "POST",
       headers: {
-        Authorization: `Bearer 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`,
+        Authorization: `Bearer ${process.env.NOTION_API_TOKEN}`,
         "Notion-Version": "2022-06-28",
         "Content-Type": "application/json",
       },
